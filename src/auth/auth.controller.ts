f3cfb7999312e4541497e91f1738b070028f41a6
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-01-09 16:32:59 +0800

    feat: 完成登录功能

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('register')
   async register(@Body() dto: RegisterDto) {
-    console.log('注册数据:', dto);
     return this.authService.register(dto);
+  }
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as argon2 from 'argon2';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -19,11 +20,29 @@ export class AuthService {
         password: await argon2.hash(dto.password),
       },
     });
-    
+
     delete user.password;
-    
+
     return {
       message: '注册成功',
+      data: user,
+    };
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findFirst({
+      where: { name: dto.name },
+    });
+
+    if (!user) {
+      throw new BadRequestException('用户不存在');
+    }
+    if (!(await argon2.verify(user.password, dto.password))) {
+      throw new BadRequestException('密码错误');
+    }
+    delete user.password;
+    return {
+      message: '登录成功',
       data: user,
     };
   }
