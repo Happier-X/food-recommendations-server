9af9ed0f21323682304a0da745b041d217810795
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-01-10 15:18:43 +0800

    fix: 调整代码

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { MulterModule } from '@nestjs/platform-express';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
-    MulterModule.register(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'uploads'),
-      serveRoot: '/uploads',
+    MulterModule.register({
+      dest: './uploads',
     }),
   ],
   controllers: [UploadController],
