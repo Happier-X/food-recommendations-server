3c738c5fb9dccdeac48850e4eecdacfb53cf4760
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-01-14 17:41:08 +0800

    feat: 完成食物删除功能

--- src/food/food.service.ts
+++ src/food/food.service.ts
@@ -15,14 +15,43 @@ export class FoodService {
     });
   }
 
-  findAll() {
-    return this.prismaService.food.findMany();
+  async findAll() {
+    let food = await this.prismaService.food.findMany();
+    // forEach是同步执行的,内部的异步操作不会等待
+    // 需要使用Promise.all来等待所有异步操作完成
+    const foodWithUser = await Promise.all(
+      food.map(async (item) => {
+        const user = await this.prismaService.user.findUnique({
+          where: {
+            id: item.userId,
+          },
+        });
+        return {
+          ...item,
+          user,
+        };
+      }),
+    );
+    return foodWithUser;
   }
 
-  findOne(id: number) {
-    return this.prismaService.food.findFirst({
-      where: { id },
+  async findOne(id: number) {
+    const food = await this.prismaService.food.findUnique({
+      where: {
+        id,
+      },
     });
+
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: food.userId,
+      },
+    });
+
+    return {
+      ...food,
+      user,
+    };
   }
 
   update(id: number, updateFoodDto: UpdateFoodDto) {
@@ -43,8 +72,8 @@ export class FoodService {
     });
   }
 
-  remove(id: number) {
-    this.prismaService.food.delete({
+  async remove(id: number) {
+    await this.prismaService.food.delete({
       where: {
         id,
       },
